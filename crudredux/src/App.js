ff5126413f1828bf13ae81988542e2e356be92f0
JorgeRod2594 <jor567.jr@gmail.com>
2020-06-27 17:56:15 -0500

    Configuración REDUx, devtools, creación del state y compatibilidad con navegadores

--- crudredux/src/App.js
+++ crudredux/src/App.js
@@ -9,21 +9,29 @@ import EditarProducto from './components/EditarProducto';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // BrowserRouter con un alias, Route que sería cada página  y Switch
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
     //En programación, el término boilerplate se refiere a bloques de código utilizados una y otra vez.
 
+    //  Al agregar provider todas las funciones que tengamos van a estar disponibles en todo el proyecto.
   return (
+      
     <Router>
-        <Header />
-            <div className="container mt-5 mb-5">
-                {/*Definimos las rutas dentro del switch. Pasamos el path y el componente que cargará al ingresar a la ruta*/}
-                <Switch>
-                    <Route exact path ="/" component= {Productos}/>
-                    <Route exact path ="/productos/nuevo" component= {NuevoProducto}/>
-                    <Route exact path ="/productos/editar/:id" component= {EditarProducto}/>
-                </Switch>
-            </div>
-        <Footer />
+        <Provider store={store}>
+            <Header />
+                <div className="container mt-5 mb-5">
+                    {/*Definimos las rutas dentro del switch. Pasamos el path y el componente que cargará al ingresar a la ruta*/}
+                    <Switch>
+                        <Route exact path ="/" component= {Productos}/>
+                        <Route exact path ="/productos/nuevo" component= {NuevoProducto}/>
+                        <Route exact path ="/productos/editar/:id" component= {EditarProducto}/>
+                    </Switch>
+                </div>
+            <Footer />
+        </Provider>
     </Router>
   );
 }
---
+++ crudredux/src/reducers/productosReducer.js
@@ -0,0 +1,17 @@
+//Cada reducer tiene su propio state
+const initialState = {
+    productos: [], //Definimos el arreglo de productos. Este se llenará cuando hagamos las consultas a la db.
+    error: null, //Para mostrar errores
+    loading: false //Para realizar la espera mientras carga los datos
+}
+
+//El store de  pasa el state y el action que ejecutará el reducer, encaso de no pasarle nada le pasamos el initialState
+//como en la siguiente linea.
+export default function(state = initialState, action) { //Cualquier reducer es un switch
+    /*Colocamos todos los cases que describen que pasará en la aplicación y van a estar cambiando el state mediante el payload.*/
+    switch(action.type) {
+        default:
+            return state;
+    }
+
+}
