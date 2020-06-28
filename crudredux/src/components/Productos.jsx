89aaa5a141315a3b0f57530bfe83b08d4841739e
JorgeRod2594 <jor567.jr@gmail.com>
2020-06-28 14:47:24 -0500

    Llamando el action desde el componente

--- crudredux/src/components/Productos.jsx
+++ crudredux/src/components/Productos.jsx
@@ -1,6 +1,23 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+
+//Redux
+import { useSelector, useDispatch } from 'react-redux';
+import { obtenerProductosAction } from '../actions/productoActions';
 
 const Productos = () => {
+
+    //Utilizamos useDispatch y crea una funcion que se utiliza en la funcion 1. 
+    const dispatch = useDispatch();
+
+    //Manda a hacer la consulta a la base de datos cuando este componente carga con useEffect
+    useEffect(() => {
+        //1. Aqui llamamos una funcion de redux que manda a llamar al action obtenerProductosAction
+        //Para poder Consultar a la api y para obtener los productos en 2.
+        const cargarProductos = () => dispatch(obtenerProductosAction() );
+        //2.Consultamos a la api para obtener los productos
+        cargarProductos();
+    }) //Le pasamos un dispatch como referencia
+
     return ( 
         <Fragment>
             <h2 className="text-center my-5">Listado de productos</h2>
